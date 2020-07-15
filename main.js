3155905378d9a3a9e5545876803d242e1425a14e
你çclang1996 <>
2020-07-16 03:01:29 +0800

    求重复次数最多的元素和重复次数

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-let arr1=[2, 21, 4, 32, 43, 1, 9];
-// 1.call/apply 方法
-console.log(Math.max.call(null, ...arr1));
-console.log(Math.min.apply(null, arr1));
-// 2.sort()排序后取两端
-let arr2=[0, 2, 21, 4, 32, 43, 1, 9, 55];
-arr2.sort((a, b) => {return a - b;});
-console.log(arr2[arr2.length - 1]);
-console.log(arr2[0]);
-//3.reduce
-let arr3=[0, 2, 21, 42, 32, 43, 1, 9, 55, 100];
-const sum=arr3.reduce((a, b) => a>b ? a : b);
-console.log(sum);
+let name=Array.from(('ADDSSASDSDSEIEKDSSESDDEWESS'))
+let count=name.reduce(function (container, item) {
+	if (item in container) {
+		container[item]++;
+	}else {
+		container[item]=1;
+	}
+	return container;
+}, {});
+console.log(count)
